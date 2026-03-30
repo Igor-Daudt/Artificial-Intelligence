1249382fcb2e46d697eef16d4b29aaddc1dd0a59
Melchior Boaretto Neto <melchiorboaretto@protonmail.com>
2026-03-29 22:26:56 -0300

    Everything works fine, q7 lacks the 7000 threshold fine tuning

--- Trabalho 1 - Busca/autograder.sh
+++ Trabalho 1 - Busca/autograder.sh
@@ -1,3 +1,14 @@
-!#/bin/zsh
+#!/bin/zsh
 
-python autograder.py -q $1
+python autograder.py -q q1 > q1.autotemp 
+python autograder.py -q q2 > q2.autotemp 
+python autograder.py -q q3 > q3.autotemp 
+python autograder.py -q q4 > q4.autotemp 
+python autograder.py -q q5 > q5.autotemp 
+python autograder.py -q q6 > q6.autotemp 
+python autograder.py -q q7 > q7.autotemp 
+python autograder.py -q q8 > q8.autotemp
+
+cat *.autotemp | grep Total: 
+
+rm *.autotemp
