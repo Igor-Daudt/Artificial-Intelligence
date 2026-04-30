e843b48fbdcdef8bd4a4f84e990b13ac653c7ef1
Igor-Daudt <igordaudt1@gmail.com>
2026-04-30 17:14:52 -0300

    othello_custom_start

--- kit_games/advsearch/your_agent/othello_minimax_custom.py
+++ kit_games/advsearch/your_agent/othello_minimax_custom.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from ..othello.gamestate import GameState
 from ..othello.board import Board
 from .minimax import minimax_move
+from .othello_minimax_mask import evaluate_mask
 
 # Voce pode criar funcoes auxiliares neste arquivo
 # e tambem modulos auxiliares neste pacote.
@@ -23,7 +24,7 @@ def make_move(state) -> Tuple[int, int]:
     # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
     # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
 
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, max_depth=3, eval_func=evaluate_custom)
 
 
 def evaluate_custom(state, player:str) -> float:
@@ -34,4 +35,28 @@ def evaluate_custom(state, player:str) -> float:
     :param state: state to evaluate (instance of GameState)
     :param player: player to evaluate the state for (B or W)
     """
-    return 0    # substitua pelo seu codigo
+    # Um fator de posicao maior da mais importancia ao mask em comparacao com count
+    fator_de_posicao = 12
+
+    if state.is_terminal():
+        # Checar valor de vitoria ou derrota
+        winner = state.winner()
+        # Checar vitoria do jogador
+        if winner == player:
+            return 10000.0
+        # Checar empate
+        elif winner is None:
+            return 0.0
+        # Checar derrota do jogador
+        else:
+            return -10000.0
+        
+    # Obtem o valor das outras heuristicas
+    valor_mask = evaluate_mask(state, player)
+    board = state.get_board()
+    valor_contagem = board.num_pieces(player)
+    valor_contagem_oponente = board.num_pieces(board.opponent(player))
+
+    valor_exploracao = (valor_contagem + valor_contagem_oponente) / fator_de_posicao
+
+    return valor_exploracao * valor_contagem + valor_mask / valor_exploracao
